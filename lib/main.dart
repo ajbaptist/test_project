80e4f4a678a66e174079901b74f57b3388b07468
JOHN BAPTIST <75603354+ajbaptist@users.noreply.github.com>
2024-05-17 10:43:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,98 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: TextButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return const Home();
+                  },
+                ));
+              },
+              child: const Text('Test')),
         ));
   }
 }
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late InAppWebViewController webViewController;
+
+  @override
+  void initState() {
+    webViewController = InAppWebViewController.fromPlatform(
+        platform: PlatformInAppWebViewController.static());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (await webViewController.canGoBack()) {
+          webViewController.goBack();
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+          appBar: AppBar(),
+          body: InAppWebView(
+            initialUrlRequest: URLRequest(url: WebUri("https://flutter.dev")),
+            onWebViewCreated: (controller) {
+              webViewController = controller;
+            },
+            shouldOverrideUrlLoading: (controller, navigationAction) async {
+              var request = navigationAction.request;
+              var url = request.url;
+              var isUrlMatching =
+                  url != null && (url.path.contains('get-started'));
+
+              if (isUrlMatching) {
+                return NavigationActionPolicy.CANCEL;
+              }
+              return NavigationActionPolicy.ALLOW;
+            },
+            onUpdateVisitedHistory: (controller, url, isReload) {
+              print('url updated-->${url?.path}');
+            },
+          )),
+    );
+  }
+}
+
